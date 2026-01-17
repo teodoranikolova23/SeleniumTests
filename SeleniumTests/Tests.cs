f6aef75ed7764adbf0db0365c122f40dbebe834a
Teodora Nikolova <teodora@InnovativeSS.onmicrosoft.com>
2026-01-17 15:07:26 +0200

    push a fix

--- SeleniumTests/Tests.cs
+++ SeleniumTests/Tests.cs
@@ -1,15 +1,11 @@
 using OpenQA.Selenium.Chrome;
 using SeleniumTests.Pages;
-using SeleniumTests.Seleinum.Core;
 using Tests;
 
 namespace SeleniumTests
 {
     public class Tests : BaseTest
     {
-        protected HomePage HomePage => new(Browser);
-        protected TyposPage TyposPage => new(Browser);
-
         [Test]
         public void TyposPage_ShouldDisplayExpectedText()
         {
